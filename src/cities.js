b807e9177337450b722b381a8e4feb97a5464af4
Ye Min Naing <thargyi74@gmail.com>
2025-01-02 14:16:17 +0700

    Add new cities and update generation time for weather data

--- src/cities.js
+++ src/cities.js
@@ -2,38 +2,48 @@ const cities = [
   {
     name: "Yangon",
     latitude: 16.75,
-    longitude: 96.125
+    longitude: 96.125,
   },
   {
     name: "Mandalay",
-    latitude: 22.00,
-    longitude: 96.125
+    latitude: 22.0,
+    longitude: 96.125,
   },
   {
     name: "Naypyitaw",
     latitude: 19.75,
-    longitude: 96.125
+    longitude: 96.125,
   },
   {
     name: "Sagaing",
     latitude: 21.875,
-    longitude: 96.00
+    longitude: 96.0,
   },
   {
     name: "Meiktila",
     latitude: 20.875,
-    longitude: 95.875
+    longitude: 95.875,
   },
   {
     name: "Pakokku",
     latitude: 21.375,
-    longitude: 95.125
+    longitude: 95.125,
   },
   {
     name: "Sittwe",
     latitude: 20.125,
-    longitude: 92.875
-  }
+    longitude: 92.875,
+  },
+  {
+    name: "Mawlamyine",
+    latitude: 16.485,
+    longitude: 97.626,
+  },
+  {
+    name: "Dawei",
+    latitude: 14.083,
+    longitude: 98.192,
+  },
 ];
 
 module.exports = {
