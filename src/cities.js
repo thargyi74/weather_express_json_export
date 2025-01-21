201973c1171872aecf9809683ccbc126584227fd
Ye Min Naing <thargyi74@gmail.com>
2025-01-21 15:26:27 +0700

    Update city coordinates and add new cities to the list

--- src/cities.js
+++ src/cities.js
@@ -1,56 +1,81 @@
 const cities = [
   {
     name: "Yangon",
-    latitude: 16.75,
-    longitude: 96.125,
+    latitude: 16.8661,
+    longitude: 96.1951,
   },
   {
     name: "Mandalay",
-    latitude: 22.0,
-    longitude: 96.125,
+    latitude: 21.9588,
+    longitude: 96.0891,
   },
   {
-    name: "Naypyitaw",
-    latitude: 19.75,
-    longitude: 96.125,
+    name: "Myitkyina",
+    latitude: 25.3833,
+    longitude: 97.3833,
   },
   {
-    name: "Sagaing",
-    latitude: 21.875,
-    longitude: 96.0,
+    name: "Hakha",
+    latitude: 22.65,
+    longitude: 93.6167,
   },
   {
-    name: "Meiktila",
-    latitude: 20.875,
-    longitude: 95.875,
+    name: "Monywa",
+    latitude: 22.1086,
+    longitude: 95.1358,
   },
   {
-    name: "Pakokku",
-    latitude: 21.375,
-    longitude: 95.125,
+    name: "Taunggyi",
+    latitude: 20.7833,
+    longitude: 97.0333,
   },
   {
     name: "Sittwe",
-    latitude: 20.125,
-    longitude: 92.875,
+    latitude: 20.1527,
+    longitude: 92.8725,
+  },
+  {
+    name: "Magway",
+    latitude: 20.15,
+    longitude: 94.95,
+  },
+  {
+    name: "Naypyidaw",
+    latitude: 19.7633,
+    longitude: 96.0785,
+  },
+  {
+    name: "Loikaw",
+    latitude: 19.6775,
+    longitude: 97.2115,
+  },
+  {
+    name: "Bago",
+    latitude: 17.335,
+    longitude: 96.4815,
+  },
+  {
+    name: "Pathein",
+    latitude: 16.7833,
+    longitude: 94.7333,
+  },
+  {
+    name: "Hpa-an",
+    latitude: 16.8891,
+    longitude: 97.6446,
   },
   {
     name: "Mawlamyine",
-    latitude: 16.485,
+    latitude: 16.491,
     longitude: 97.626,
   },
   {
     name: "Dawei",
-    latitude: 14.083,
+    latitude: 14.0823,
     longitude: 98.192,
-  },
-  {
-    name: "Chiang Mai",
-    latitude: 18.7883,
-    longitude: 98.9853,
   },
 ];
 
 module.exports = {
-  cities
+  cities,
 };
